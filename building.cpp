ae23e56aa78bed08f7e2836c16303e91fc108aef
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-23 03:01:12 -0500

    building location

--- building.cpp
+++ building.cpp
@@ -8,6 +8,7 @@
 // CS 211
 // 
 
+#include <utility>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -41,15 +42,17 @@ void Building::print(Nodes& nodes) {
     cout << this->Name << endl;
     cout << "Address: " << this->StreetAddress << endl;
     cout << "Building ID: " << this->ID << endl;
-    cout << "# perimeter nodes: " << this->NodeIDs.size();
+    cout << "# perimeter nodes: " << this->NodeIDs.size() << endl; 
+    pair<double,double> P = this->getLocation(nodes);
+    cout << "Location: (" << P.first << ", " << P.second << ")" << endl;
     // cout << "Nodes:" << endl;
-    for (long long nodeid : this->NodeIDs)
-    {
+    // for (long long nodeid : this->NodeIDs)
+    // {
       // cout << "  " << nodeid << ": ";
 
-      double lat = 0.0;
-      double lon = 0.0;
-      bool entrance = false;
+      // double lat = 0.0;
+      // double lon = 0.0;
+      // bool entrance = false;
 
       // bool foundNode = nodes.find(nodeid, lat, lon, entrance);
 
@@ -64,6 +67,34 @@ void Building::print(Nodes& nodes) {
       // else {
         // cout << "**NOT FOUND**" << endl;
       // }
-    }//for
+    // }//for
+}
+
+pair<double,double> Building::getLocation(Nodes& nodes) {
+    double avgLat = 0;
+    double avgLon = 0;
+    double parameter = this->NodeIDs.size();
+    for (long long nodeid : this->NodeIDs) {
+      double lat = 0.0;
+      double lon = 0.0;
+      bool entrance = false;
+
+      bool foundNode = nodes.find(nodeid, lat, lon, entrance);
+
+      if (foundNode) {
+        // 
+        // If node exist, then add up the lon and lat
+        //
+        avgLat = avgLat + lat;
+        avgLon = avgLon + lon;
+       } //if
+      } //for
+    
+    // find average
+    avgLat = avgLat / parameter;
+    avgLon = avgLon / parameter;
+
+return make_pair(avgLat,avgLon);
+
 }
 
