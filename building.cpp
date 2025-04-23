d3ead68df415ec1888fd07b9bd94a735237bc79f
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-23 02:08:52 -0500

    helper function building:print()

--- building.cpp
+++ building.cpp
@@ -8,7 +8,13 @@
 // CS 211
 // 
 
+#include <string>
+#include <vector>
+#include <iostream>
+
 #include "building.h"
+#include "node.h"
+#include "nodes.h"
 
 using namespace std;
 
@@ -28,5 +34,36 @@ Building::Building(long long id, string name, string streetAddr)
 void Building::add(long long nodeid)
 {
   this->NodeIDs.push_back(nodeid);
+}
+
+void Building::print(Nodes& nodes) {
+
+    cout << this->Name << endl;
+    cout << "Address: " << this->StreetAddress << endl;
+    cout << "Building ID: " << this->ID << endl;
+
+    cout << "Nodes:" << endl;
+    for (long long nodeid : this->NodeIDs)
+    {
+      cout << "  " << nodeid << ": ";
+
+      double lat = 0.0;
+      double lon = 0.0;
+      bool entrance = false;
+
+      bool foundNode = nodes.find(nodeid, lat, lon, entrance);
+
+      if (foundNode) {
+        cout << "(" << lat << ", " << lon << ")";
+
+        if (entrance)
+          cout << ", is entrance";
+
+        cout << endl;
+      }
+      else {
+        cout << "**NOT FOUND**" << endl;
+      }
+    }//for
 }
 
