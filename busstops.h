59f9f703389c7397dd676bacbfe674abe0838682
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-24 00:11:50 -0500

    Closest busstop, south and north bound

---
+++ busstops.h
@@ -0,0 +1,41 @@
+/*bustops.h*/
+
+//
+// Take in and parse each line of the bus stop CVS file
+// 
+// Rocelyn Young
+// Northwestern University
+// CS 211
+// 
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <utility>
+
+#include "busstop.h"
+
+using namespace std;
+
+class BusStops
+{
+    public:  
+    vector<BusStop> vecBusStops;
+    //
+    // constructor
+    //
+    // Given an CVS document, reads through the document and 
+    // parse each line
+    //
+
+    BusStops();
+
+    void print();
+
+    pair<BusStop,double> SouthClosestStop(double place_lat,double place_lon);
+
+    pair<BusStop,double> NorthClosestStop(double building_lat, double building_lon);
+
+};
