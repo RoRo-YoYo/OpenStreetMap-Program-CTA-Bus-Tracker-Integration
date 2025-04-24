bd6c9fcd220fb37d256b7fad56f1cefdbc432a0d
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-24 17:16:08 -0500

    added detailed comment and fix memory leak

--- busstop.h
+++ busstop.h
@@ -1,7 +1,7 @@
 /*bustop.h*/
 
 //
-// Take in and parse each line of the bus stop CVS file
+// A bus in a CVS file
 // 
 // Rocelyn Young
 // Northwestern University
@@ -36,11 +36,14 @@ class BusStop
     // Build a busstop
     // 
     //
-
+    // Each bus stop consists of a stop ID (15924), the bus route (201), the stop name (Central & Sheridan), the direction of travel (Eastbound), the location of the stop (SW
+    // corner), and the position in latitude and longitude
     BusStop(int id, int route, string stopname, string direction, string location, double lat, double lon);
 
+    // print the nearest busstop, along with the prediction of incoming bus
     void print(double miles,CURL* curl);
 
+    // // Find the prediction given curl, url, and string to store the information found. Print out, if exist. Else, catch error if not.
     void printBusPrediction(CURL* curl, string url, string& response);
 
 };
