db9fbcf2ab186e8625fbf5691814137f56b0d4a7
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-23 23:00:34 -0500

    printing bus stop

--- busstop.cpp
+++ busstop.cpp
@@ -7,8 +7,6 @@
 // Northwestern University
 // CS 211
 // 
-#pragma once
-
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -21,31 +19,14 @@ using namespace std;
     //
     // constructor
     //
-    // Given an CVS document, reads through the document and 
-    // parse each line
-    //
 
-    BusStop::BusStop(string filename) {
-        ifstream infile;
-        infile.open(filename); //Open file
 
-        string line;
-        getline(infile, line); // read a line from the input file
+    BusStop::BusStop(int id, int route, string stopname, string direction, string location, double lat, double lon) // Create busstop and initialize variable
+    : ID(id), Route(route), StopName(stopname),Direction(direction),Location(location),Lat(lat),Lon(lon)
+    
+    {
 
-        stringstream parser(line); // setup to parse the line
+    }
 
-        getline(parser, id_str, ',');// extract each value:
-        getline(parser, route_str, ',');
-        getline(parser, stopname, ',');
-        getline(parser, direction, ',');
-        getline(parser, location, ',');
-        getline(parser, lat_str, ',');
-        getline(parser, lon_str);
 
-        // Type correction
-        id_str = stoi(id_str);  //int
-        route_str = stoi(route_str); 
-        lat_str = stod(lat_str);  // Double
-        lon_str = stod(lon_str);
- };
 
