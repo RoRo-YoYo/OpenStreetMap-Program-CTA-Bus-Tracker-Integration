6151ab1438994e620c323dc853820cf99e6fb147
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-24 16:07:36 -0500

    Completed prediction

--- busstop.cpp
+++ busstop.cpp
@@ -11,12 +11,14 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
+
 
 #include "busstop.h"
-#include "curl_util.h"
 #include "json.hpp"
 #include "node.h"
 #include "nodes.h"
+
 
 
 using namespace std;
@@ -35,24 +37,39 @@ using json = nlohmann::json;
 
     }
 
-    void BusStop::print(pair<pair<BusStop,double>,pair<BusStop,double>> ) {
+    void BusStop::print(double miles, CURL* curl) {
+
     // //
     // // Find and Print closest south bound. Set up url
     // //
 
-    if 
+    if  (this->Direction == "Southbound") {
+        cout << "Closest southbound bus stop:" << endl;
+        cout << "  " << this->ID << ": " << this->StopName << ", bus #" << this->Route << ", " << this->Location << ", " << miles << " miles" << endl;
+        string southurl = "http://ctabustracker.com/bustime/api/v2/getpredictions?key=jehZ84bN7qAcqWhWj8Jzvs9qE&rt=" + to_string(this->Route) + "&stpid=" + to_string(this->ID) + "&format=json";
 
-    // pair<BusStop,double> SouthDistance = busstops.SouthClosestStop(P.first, P.second);
-    // cout << "Closest southbound bus stop:" << endl;
-    // cout << " " << SouthDistance.first.ID << ":" << SouthDistance.first.StopName << ", bus #" << SouthDistance.first.Route << ", " << SouthDistance.first.Location << ", " << SouthDistance.second << " miles" << endl;
-    // string southurl = "http://ctabustracker.com/bustime/api/v2/getpredictions?key=jehZ84bN7qAcqWhWj8Jzvs9qE&rt=" + to_string(SouthDistance.first.Route) + "&stpid=" + to_string(SouthDistance.first.ID) + "&format=json";
+        string southresponce;
+        this->printBusPrediction(curl,southurl,southresponce);
 
-    // // string southresponce;
-    // // SouthDistance.first.printBusPrediction(CURL* curl, southurl, southresponce);
+    }
 
     // //
     // // Find and Print closest nouth bound. Set up url
-    // //
+    // //   
+
+    else if (this->Direction == "Northbound") {
+        cout << "Closest northbound bus stop:" << endl;
+        cout << "  " << this->ID << ": " << this->StopName << ", bus #" << this->Route << ", " << this->Location << ", " << miles << " miles" << endl;
+        string northurl = "http://ctabustracker.com/bustime/api/v2/getpredictions?key=jehZ84bN7qAcqWhWj8Jzvs9qE&rt=" + to_string(this->Route) + "&stpid=" + to_string(this->ID) + "&format=json";
+
+        string northresponce;
+        this->printBusPrediction(curl, northurl,northresponce);
+    } }
+
+     // pair<BusStop,double> SouthDistance = busstops.SouthClosestStop(P.first, P.second);
+    
+
+
     // pair<BusStop,double> NorthDistance = busstops.NorthClosestStop(P.first, P.second);
     // cout << "Closest southbound bus stop:" << endl;
     // cout << " " << NorthDistance.first.ID << ":" << NorthDistance.first.StopName << ", bus #" << NorthDistance.first.Route << ", " << NorthDistance.first.Location << ", " << NorthDistance.second << " miles" << endl;
@@ -61,21 +78,24 @@ using json = nlohmann::json;
     // // string southresponce;
     // // SouthDistance.first.printBusPrediction(CURL* curl, southurl, southresponce);
 
-    }
-
-    // void BusStop::printBusPrediction(CURL* curl, string url, string& response) {
-    //     if (callWebServer(curl, url, response) == true) {
-    //        auto jsondata = json::parse(response);
-    //        auto bus_response = jsondata["bustime-response"];
-    //        auto predictions = bus_response["prd"];
+    void BusStop::printBusPrediction(CURL* curl, string url, string& response) {
+        if (callWebServer(curl, url, response) == true) {
+           auto jsondata = json::parse(response);
+           auto bus_response = jsondata["bustime-response"];
+           auto predictions = bus_response["prd"];
       
-    //        // for each prediction (a map) in the prediction list
-    //        for (auto& M : predictions) {
-    //            cout << "  vehicle #" << stoi(M["vid"].get_ref<string&>()) << "on the route " << stoi(M["rt"].get_ref<string&>()) << " travelling " << M["rtdir"].get_ref<string&>() << "to arrive in " << stoi(M["prdctdn"].get_ref<string&>()) << " mins" << endl;}
-    //     }
-    //     else {
-    //       cout << " << bus predictions unavailable, call failed>>" << endl;}
-    //   }
+           // for each prediction (a map) in the prediction list 
+           for (auto& M : predictions) { 
+            try {
+               cout << "  vehicle #" << stoi(M["vid"].get_ref<string&>()) << " on the route " << stoi(M["rt"].get_ref<string&>()) << " travelling " << M["rtdir"].get_ref<string&>() << " to arrive in " << stoi(M["prdctdn"].get_ref<string&>()) << " mins" << endl;}
+            //
+            catch (exception& e) {
+                cout << " error" << endl;
+                cout << " malformed CTA response, prediction unavailable"
+                     << " << bus predictions unavailable, call failed>>" << endl;}
+            } //for
+
+      }}
       
 
 
